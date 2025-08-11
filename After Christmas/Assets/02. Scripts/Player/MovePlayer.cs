043da68c48a34ac8c25cfb05cc53211e13ec4e23
gjsk132 <gjsk132@naver.com>
2025-08-11 21:07:24 +0900

    [feature] 플레이어 이동 구현

--- After Christmas/Assets/02. Scripts/Player/MovePlayer.cs
+++ After Christmas/Assets/02. Scripts/Player/MovePlayer.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+
+    // 45도 쿼터뷰 기준 전방/오른쪽 벡터
+    private Vector3 isoForward = new Vector3(1, 0, 1).normalized;
+    private Vector3 isoRight = new Vector3(1, 0, -1).normalized;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation; // 회전 고정
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // 입력 방향을 쿼터뷰 좌표로 변환
+        Vector3 moveDir = isoForward * v + isoRight * h;
+
+        // 정규화해서 대각선 속도 보정
+        if (moveDir.sqrMagnitude > 1f)
+            moveDir.Normalize();
+
+        // Rigidbody 이동
+        rb.linearVelocity = moveDir * moveSpeed + new Vector3(0, rb.linearVelocity.y, 0);
     }
 }
