e4c0b719e450657044dfe631f2505b8cb2ff99ac
gjsk132 <gjsk132@naver.com>
2025-09-04 15:20:05 +0900

    [feature] item dialogue

---
+++ After Christmas/Assets/02. Scripts/Dialogue/DialogueTest.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DialogueTest : MonoBehaviour
+{
+    public DialogueData testDialogue;
+    public CharacterData testCharacter;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (testDialogue != null && DialogueManager.Instance != null)
+            {
+                DialogueManager.Instance.StartDialogue(testDialogue, testCharacter);
+            }
+            else
+            {
+                Debug.LogWarning("warning : 컴포넌트 세팅 다시하기");
+            }
+        }
+    }
+}
