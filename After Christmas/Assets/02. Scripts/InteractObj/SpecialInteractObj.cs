0fd137a7f3fed129baeb8d5b7b5ce4e98c1fa0a5
고혁빈 <rhgurqls00@gmail.com>
2025-10-19 13:05:12 +0900

    [Feature] 특수 상호작용 재시도 방지 로직 추가

--- After Christmas/Assets/02. Scripts/InteractObj/SpecialInteractObj.cs
+++ After Christmas/Assets/02. Scripts/InteractObj/SpecialInteractObj.cs
@@ -14,6 +14,9 @@ public class SpecialInteractObj : MonoBehaviour, IInteractable, IGlowable
 
     private DialogueTrigger dialogueTrigger;
 
+    // 최초 상호작용인지가 아닌, 특수 상호작용이 완료되었는지 확인하는 변수(한개로 여러번 상호작용 방지)
+    private bool isSpecialInteracted = false;
+
     private bool firstInteraction = true;
 
     private void Awake()
@@ -58,10 +61,14 @@ public class SpecialInteractObj : MonoBehaviour, IInteractable, IGlowable
         // 3. 대화 (index 2) → 끝나면 SpecialInteraction 실행
         // 이 부분 애매한게, 대화 -> 균열 메우기일지 / 균열 메우기 -> 대화 일지 순서 모호
         // 플레이상 자연스러우려면 균열을 메우고 대화 진행일 텐데, 그러면 콜백 안써도 될듯
-        dialogueTrigger.StartDialogueAtIndex(2, () =>
+        if (isSpecialInteracted)
+        {
+            dialogueTrigger.StartDialogueAtIndex(2, () =>
         {
             SpecialInteraction();
         });
+        }
+
     }
 
     private bool IsSpecialInteractable(GameObject player)
@@ -71,6 +78,7 @@ public class SpecialInteractObj : MonoBehaviour, IInteractable, IGlowable
 
     private void SpecialInteraction()
     {
+        isSpecialInteracted = true;
         interactWithItem?.Invoke();
     }
 }
