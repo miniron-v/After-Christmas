5ae9bfca5051e9cde3c97534faae8e174c05eed3
고혁빈 <rhgurqls00@gmail.com>
2025-12-24 23:34:24 +0900

    [Feature] 버튼 생성 로직 임시 구현
    rotation 손봐야 함

---
+++ After Christmas/Assets/02. Scripts/UI/MemoPanel.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.EventSystems; // 클릭 이벤트를 받기 위해 필요
+
+public class UIObjectSpawner : MonoBehaviour, IPointerClickHandler
+{
+    [Header("생성할 버튼/포스트잇 프리팹")]
+    [SerializeField] private GameObject buttonPrefab;
+
+    [Header("생성된 버튼들이 담길 부모 (미지정 시 이 패널의 자식으로 생성)")]
+    [SerializeField] private Transform container;
+
+    private void Awake()
+    {
+        if (container == null)
+            container = this.transform;
+    }
+
+    // 패널이 클릭되었을 때 실행되는 함수
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // 1. 클릭된 월드 좌표 가져오기
+        Vector3 clickWorldPos = eventData.pointerCurrentRaycast.worldPosition;
+
+        // 2. 프리팹 생성
+        GameObject newButton = Instantiate(buttonPrefab, container);
+
+        // 3. 위치 설정
+        newButton.transform.position = clickWorldPos;
+
+        // 4. UI가 겹치지 않게 패널 앞으로 살짝 띄우기
+        newButton.transform.localPosition += new Vector3(0, 0.001f, 0); 
+
+        Debug.Log($"버튼 생성 위치: {clickWorldPos}");
+    }
+}
