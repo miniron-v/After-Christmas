2c15bc5733b2f616d626c1038855568d163aab0c
고혁빈 <rhgurqls00@gmail.com>
2026-01-03 11:43:27 +0900

    [Fix] 텔레포트 오류 수정

--- After Christmas/Assets/02. Scripts/Player/MovePlayer.cs
+++ After Christmas/Assets/02. Scripts/Player/MovePlayer.cs
@@ -13,17 +13,15 @@ public class MovePlayer : MonoBehaviour
     [Header("Animation")]
     [SerializeField] private Animator animator;
 
+
     private Vector2 moveInput;
     private Rigidbody rb;
-
     private readonly Vector3 isoForward = new Vector3(1, 0, 1).normalized;
     private readonly Vector3 isoRight = new Vector3(1, 0, -1).normalized;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-        rb.interpolation = RigidbodyInterpolation.Interpolate;
     }
 
     public void OnMove(InputValue value)
@@ -31,15 +29,23 @@ public class MovePlayer : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (!PlayerStateManager.Instance.IsPlayerControllable())
+        {
+            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
             return;
+        }
 
         Vector3 moveDir = isoForward * moveInput.y + isoRight * moveInput.x;
-        if (moveDir.sqrMagnitude > 1f) moveDir.Normalize();
+        if (moveDir.sqrMagnitude > 1f)
+            moveDir.Normalize();
+
+        Vector3 targetlinearVelocity = moveDir * moveSpeed;
+        rb.linearVelocity = new Vector3(targetlinearVelocity.x, rb.linearVelocity.y, targetlinearVelocity.z);
 
         bool isMoving = moveInput.sqrMagnitude > 0.01f;
+
 
         if (moveDir.sqrMagnitude > 0.0001f)
         {
@@ -52,22 +58,5 @@ public class MovePlayer : MonoBehaviour
         }
 
         animator.SetBool("isMoving", isMoving); 
-    }
-
-    private void FixedUpdate()
-    {
-        if (!PlayerStateManager.Instance.IsPlayerControllable())
-        {
-            // 컨트롤 불가능할 때 미끄러지지 않도록 속도 제어
-            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
-            return;
-        }
-
-        Vector3 moveDir = isoForward * moveInput.y + isoRight * moveInput.x;
-        if (moveDir.sqrMagnitude > 1f) moveDir.Normalize();
-
-        // Velocity 기반 이동으로 변경
-        Vector3 targetVelocity = moveDir * moveSpeed;
-        rb.linearVelocity = new Vector3(targetVelocity.x, rb.linearVelocity.y, targetVelocity.z);
     }
 }
