57b8ca926957878f7dee2dbbd95d18ebc30b3b97
고혁빈 <rhgurqls00@gmail.com>
2026-01-07 20:22:23 +0900

    [Fix] 플레이어 떨림 수정

--- After Christmas/Assets/02. Scripts/Manager/PlayerCameraFollowManager.cs
+++ After Christmas/Assets/02. Scripts/Manager/PlayerCameraFollowManager.cs
@@ -5,40 +5,30 @@ public class PlayerCameraFollowManager : MonoBehaviour
 {
     [Header("Camera Follow Settings")]
     public Camera targetCamera;          // 따라오는 카메라
-    public float smoothSpeed = 5f;
+    public float smoothTime = 0.125f;
 
     private Vector3 cameraOffset;
+    private Vector3 currentVelocity = Vector3.zero;
 
     private void Awake()
     {
-        if (targetCamera == null)
-            targetCamera = Camera.main;
+        if (targetCamera == null) targetCamera = Camera.main;
 
-        if (targetCamera == null)
-        {
-            Debug.LogError("PlayerCameraFollowManager: targetCamera가 설정되지 않았습니다.");
-            return;
-        }
-
-        // Player = this.transform
         cameraOffset = targetCamera.transform.position - transform.position;
     }
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
         if (PlayerStateManager.Instance.currentState == PlayerState.MiniMap)
-        {
-            return;
-        }
-        if (targetCamera == null)
             return;
 
         Vector3 desiredPos = transform.position + cameraOffset;
 
-        targetCamera.transform.position = Vector3.Lerp(
+        targetCamera.transform.position = Vector3.SmoothDamp(
             targetCamera.transform.position,
             desiredPos,
-            smoothSpeed * Time.deltaTime
+            ref currentVelocity,
+            smoothTime
         );
     }
 }
