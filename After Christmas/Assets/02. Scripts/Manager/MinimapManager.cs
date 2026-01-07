4901d45615bbc91bff2422ebe2d346069683efc0
고혁빈 <rhgurqls00@gmail.com>
2026-01-07 16:08:46 +0900

    [Fix] 맵 전부 활성화되는 버그 수정

--- After Christmas/Assets/02. Scripts/Manager/MinimapManager.cs
+++ After Christmas/Assets/02. Scripts/Manager/MinimapManager.cs
@@ -203,7 +203,7 @@ public class MinimapManager : MonoBehaviour
     }
 
 
-    private void MoveToOriginal(Vector3 targetCameraPosition, Vector3 targetPlayerPosition, bool isMapMove, Map newMap = null)
+    private void MoveToOriginal(Vector3 targetCameraPosition, Vector3 targetPlayerPosition, bool isMapMove)
     {
         isTweening = true;
         hoveredMap = null;
@@ -238,10 +238,7 @@ public class MinimapManager : MonoBehaviour
                 EnableAllMapObjects();
                 PlayerStateManager.Instance.SetState(PlayerState.Play);
                 CloseAllPostIts();
-                if(newMap != null)
-                {
-                    DisableOtherMap();
-                }
+                DisableOtherMap();
             });
     }
 
@@ -411,7 +408,7 @@ public class MinimapManager : MonoBehaviour
             {
                 // Map 클릭 시, 다른 맵이라면 해당 Map으로 텔레포트 처리
                 MapInfoManager.Instance.currentMap = map.mapName;
-                MoveToOriginal(map.cameraSpawnPoint.position, map.playerSpawnPoint.position, true, map);
+                MoveToOriginal(map.cameraSpawnPoint.position, map.playerSpawnPoint.position, true);
             }
             else
             {
