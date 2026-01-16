b14fc0886560ea00e924bd9577b4e793aa217ecd
김민철 <61517039+miniron-v@users.noreply.github.com>
2026-01-16 17:20:04 +0900

    획득 알림 방식 변경
    - 오른쪽에 잠깐 나타났다 사라지게

--- After Christmas/Assets/02. Scripts/InteractObj/ItemAcquiredUI.cs
+++ After Christmas/Assets/02. Scripts/InteractObj/ItemAcquiredUI.cs
@@ -11,19 +11,25 @@ public class ItemAcquiredUI : MonoBehaviour
     public void Awake()
     {
         text.gameObject.SetActive(false);
+        text.rectTransform.DOAnchorPosX(530f, 0f);
     }
 
     public IEnumerator NoticeItemAcquired(string itemName)
     {
         text.text = $"'{itemName} 획득'";
 
-        text.color = new Color(1, 1, 1, 0);
+        //text.color = new Color(1, 1, 1, 0);
+        text.rectTransform.DOAnchorPosX(530f, 0f);
         text.gameObject.SetActive(true);
 
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(text.DOFade(1f, duration)).SetEase(Ease.OutCubic)
-            .Append(text.DOFade(0f, duration)).SetEase(Ease.InCubic);
+        sequence.Append(text.rectTransform.DOAnchorPosX(-50f, duration).SetEase(Ease.OutCubic))
+            .Append(text.rectTransform.DOAnchorPosX(530f, duration).SetEase(Ease.InCubic).SetDelay(duration / 2));
+        //sequence.Append(text.DOFade(1f, duration)).SetEase(Ease.OutCubic)
+        //    .Append(text.DOFade(0f, duration)).SetEase(Ease.InCubic);
 
         yield return sequence.WaitForCompletion();
+
+        text.gameObject.SetActive(false);
     }
 }
