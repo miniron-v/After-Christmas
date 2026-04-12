d27e6b20347e10ebb321e43bdf4b6414030bc9d3
gjsk132 <gjsk132@naver.com>
2026-04-12 18:05:08 +0900

    [Refactor] 레거시 정리 및 레이아웃 형태 추가
    - 여러 개의 씬으로 관리하던 Dialogue 씬 정리
    - 다양한 대화창 버전 중 작은 버전을 하나 추가

--- After Christmas/Assets/02. Scripts/Dialogue/CharacterData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using static DialogueData;
-
-[CreateAssetMenu(fileName = "CharacterData", menuName = "Dialogue System/CharacterData")]
-public class CharacterData : ScriptableObject
-{
-    public string characterName;
-    public Sprite characterImage;
-}
